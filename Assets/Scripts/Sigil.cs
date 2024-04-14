4ac09f211620a156a6ea7f63c2440a2634d222ec
CJ Nelson <CJNelson1@gmail.com>
2024-04-13 19:36:12 -0500

    Added clear check times and another sigil

--- Assets/Scripts/Sigil.cs
+++ Assets/Scripts/Sigil.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,8 @@ public class Sigil : MonoBehaviour
     public int checkpointTolerance;
     public float checkpointRadius;
     public List<Checkpoint> checkpoints;
+
+    public float goldTime; //In seconds
 
 
     void Start()
